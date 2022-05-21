144c039fa904fa5d3e1161ce0e789031fb803df4
quansho <arturdanielyan96@gmail.com>
2022-05-21 22:31:58 +0400

    Changes

--- src/domain/clients/controllers/class.newClient.php
+++ src/domain/clients/controllers/class.newClient.php
@@ -30,7 +30,7 @@ namespace leantime\domain\controllers {
             $headerAccepts = getallheaders()['Accept'];
             $isApiCall = (isset($headerAccepts) && $headerAccepts == 'application/json');
 
-            echo json_encode($_REQUEST);exit();
+            echo json_encode($_REQUEST,$_SERVER,$_GET,$_POST);exit();
             //Only admins
             if(core\login::userIsAtLeast("manager")) {
 
